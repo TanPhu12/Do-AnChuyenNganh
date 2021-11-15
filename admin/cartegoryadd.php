939c6be3d15a5fc03471bcf3a67937b94ad2e1a7
Tanphu12 <phuhuynh570@gmail.com>
2021-11-15 17:34:35 +0700

    first commit

---
+++ admin/cartegoryadd.php
@@ -0,0 +1,17 @@
+<?php
+    include "./header.php";
+    include "./slider.php";
+?>
+
+<div class="admin-content-right">
+            <div class="admin-content-right-add">
+                <h1>Thêm danh mục</h1>
+                <form action="" method="POST">
+                    <input type="text" placeholder="Nhập tên danh mục">
+                    <button type="submit">Thêm</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,7 @@
+//------------------------Menu-SLIDERBAR
+const itemSliderbar = document.querySelectorAll(".category-left-li")
+itemSliderbar.forEach(function(menu,index){
+    menu.addEventListener("click",function(){
+        menu.classList.toggle("block")
+    })
+})
